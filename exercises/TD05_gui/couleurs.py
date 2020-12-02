8c0e4c8a1ab016e6c69a0b385aea71b1d71fef63
Pauline HOSTI <pauline.hosti@ens.uvsq.fr>
2020-12-02 19:30:21 +0100

    top

--- exercises/TD05_gui/couleurs.py
+++ exercises/TD05_gui/couleurs.py
@@ -10,22 +10,21 @@ def get_color(r, g, b):
 
 def ecran_aleatoire():
     couleurs = ["blue", "green", "black", "yellow", "magenta", "red"]
-    for i in range(256):
-        for j in range(256):
-            draw_pixel(i, j, couleurs[0])
+    for i in range(128):
+        for j in range(128):
+            draw_pixel(i, j, couleurs[rd.randint(0,5)])
 
+label1 = tk.Button(couleur, text="Aléatoire", font=("helvetica","16"), fg="green", bg="white", padx=17, command=ecran_aleatoire)
+label2 = tk.Button(couleur, text="Dégradé gris", font=("helvetica","16"), fg="green", bg="white", padx=17)
+label3 = tk.Button(couleur, text="Dégradé 2D", font=("helvetica","16"), fg="green", bg="white", padx=17)
+label4 = tk.Canvas(couleur, width=256, height=256, bg="black")
 
-label1 = tk.Button(couleur, text = "Aléatoire", font=("helvetica","16"), fg = "green", bg = "white",padx = 17, command = ecran_aleatoire)
-label2 = tk.Button(couleur, text = "Dégradé gris", font=("helvetica","16"), fg = "green", bg = "white",padx = 17)
-label3 = tk.Button(couleur, text = "Dégradé 2D", font=("helvetica","16"), fg = "green", bg = "white",padx = 17)
-label4 = tk.Canvas(couleur, width = 256, height = 256, bg = "black")
-
-label1.grid(column = 0, row = 1, rowspan = 4)
-label2.grid(column = 0, row = 5, rowspan = 4)
-label3.grid(column = 0, row = 9, rowspan = 4)
-label4.grid(column = 1, row = 1, columnspan = 12, rowspan = 12)
+label1.grid(column=0, row=1, rowspan=4)
+label2.grid(column=0, row=5, rowspan=4)
+label3.grid(column=0, row=9, rowspan=4)
+label4.grid(column=1, row=1, columnspan=12, rowspan=12)
 
 def draw_pixel(i, j, color):
-    return label4.create_rectangle(i,j,i+12,j+12, fill = color)
+    return label4.create_rectangle(((i*2), (j*2)), ((i+1)*2, (j+1)*2), fill=color)
 
 couleur.mainloop()
