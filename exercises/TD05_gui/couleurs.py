bc05834d4c1cf5041c8d82129813e914eebaf828
Pauline HOSTI <pauline.hosti@ens.uvsq.fr>
2020-12-03 14:27:10 +0100

    03/12/2020

--- exercises/TD05_gui/couleurs.py
+++ exercises/TD05_gui/couleurs.py
@@ -10,21 +10,31 @@ def get_color(r, g, b):
 
 def ecran_aleatoire():
     couleurs = ["blue", "green", "black", "yellow", "magenta", "red"]
-    for i in range(128):
-        for j in range(128):
+    for i in range(256):
+        for j in range(256):
             draw_pixel(i, j, couleurs[rd.randint(0,5)])
 
-label1 = tk.Button(couleur, text="Aléatoire", font=("helvetica","16"), fg="green", bg="white", padx=17, command=ecran_aleatoire)
-label2 = tk.Button(couleur, text="Dégradé gris", font=("helvetica","16"), fg="green", bg="white", padx=17)
-label3 = tk.Button(couleur, text="Dégradé 2D", font=("helvetica","16"), fg="green", bg="white", padx=17)
-label4 = tk.Canvas(couleur, width=256, height=256, bg="black")
+def degrade_gris():
+    for i in range(256):
+        for j in range(256):
+                draw_pixel(i, j, get_color(i,i,i))
 
-label1.grid(column=0, row=1, rowspan=4)
-label2.grid(column=0, row=5, rowspan=4)
-label3.grid(column=0, row=9, rowspan=4)
-label4.grid(column=1, row=1, columnspan=12, rowspan=12)
+def degrade_2D():
+    for i in range(256):
+        for j in range(256):
+                draw_pixel(i, j, get_color(i,0,j))
+
+label1 = tk.Button(couleur, text = "Aléatoire", font=("helvetica","16"), fg = "green", bg = "white",padx = 17, command = ecran_aleatoire)
+label2 = tk.Button(couleur, text = "Dégradé gris", font=("helvetica","16"), fg = "green", bg = "white",padx = 17, command = degrade_gris)
+label3 = tk.Button(couleur, text = "Dégradé 2D", font=("helvetica","16"), fg = "green", bg = "white",padx = 17, command = degrade_2D)
+label4 = tk.Canvas(couleur, width = 255, height = 255, bg = "black")
+
+label1.grid(column = 0, row = 1, rowspan = 4)
+label2.grid(column = 0, row = 5, rowspan = 4)
+label3.grid(column = 0, row = 9, rowspan = 4)
+label4.grid(column = 1, row = 1, columnspan = 12, rowspan = 12)
 
 def draw_pixel(i, j, color):
-    return label4.create_rectangle(((i*2), (j*2)), ((i+1)*2, (j+1)*2), fill=color)
+    return label4.create_line((i,j),((i+1),j), fill = color)
 
 couleur.mainloop()
