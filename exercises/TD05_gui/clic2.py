27ec95005b695abbad895299171077714288b4a3
Pauline HOSTI <pauline.hosti@ens.uvsq.fr>
2021-01-09 11:41:47 +0100

    blabla

--- exercises/TD05_gui/clic2.py
+++ exercises/TD05_gui/clic2.py
@@ -3,13 +3,22 @@ import tkinter as tk
 clic = tk.Tk()
 clic.title("exercice 6")
 
+nb_clic = 0
+clic_x = []
+clic_y = []
+
 def Coordonnees(event):
-    x = event.x
-    y = event.y
-    if x < 250 : 
-        return label1.create_oval((x-25,y+25),(x+25,y-25), fill="blue")
-    else : 
-        return label1.create_oval((x-25,y+25),(x+25,y-25), fill="red")
+    global nb_cliq, clic_x, clic_y
+    clic_x.append(event.x)
+    clic_y.append(event.y)
+    if nb_clic != 2 and nb_clic != 4 :
+        nb_clic +=1 
+    elif nb_clic == 2: 
+        nb_clic +=1
+        return canvas.create_line(clic_x[0],clic_y[0],clic_x[1],clic_y[1], fill="green")
+    elif nb_clic == 4 :  
+        nb_clic +=1
+        return canvas.create_line(clic_x[2],clic_y[2],clic_x[3],clic_y[3], fill="yellow")
 
 label1 = tk.Canvas(clic, heigh=500, width=500, bg="black")
 ligne = label1.create_line((250,0),(250,500), fill="white")
