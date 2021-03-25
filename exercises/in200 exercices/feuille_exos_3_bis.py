df524109c06ea2f918d17ac27a7c22e4c395bf9f
Pauline HOSTI <pauline.hosti@ens.uvsq.fr>
2021-03-25 18:14:50 +0100

    stop

--- exercises/in200 exercices/feuille_exos_3_bis.py
+++ exercises/in200 exercices/feuille_exos_3_bis.py
@@ -22,6 +22,31 @@ def creer_tableau(n):
     return L_final
 
 
+def creer_tableau_2():
+    choix = input("taper 'nouveau' si vous souhaitez charger un nouveau tableau et taper 'charger' sinon : ")
+    if choix == "charger":
+        nom_fichier = input("entrer le nom du fichier (ne pas oublier .txt) : ")
+        chargement = open(nom_fichier, "r")
+        list_chargement = [(line.strip()).split() for line in chargement]
+        chargement.close()
+        afficher_tableau(list_chargement)
+    elif choix == "nouveau":
+        n = input("entrer la taille du tableau (un entier) : ")
+        tableau = creer_tableau(int(n))
+        afficher_tableau(tableau)
+
+
+def somme(tab):
+    for k in range(len(tab)-2):
+        for i in range(len(tab)-2):
+            tab[len(tab)-1][k] += tab[i][k]
+            tab[k][len(tab)-1] += tab[k][i]
+    return tab
+
+
+######### afficher sous forme de tableau ############################
+
+
 def afficher_tableau(tab):
     """ affiche la liste de liste sous la forme d'un tableau """
     for elem in tab:
@@ -35,12 +60,7 @@ def afficher_tableau(tab):
                 print(elem[i])
 
 
-def somme(tab):
-    for k in range(len(tab)-2):
-        for i in range(len(tab)-2):
-            tab[len(tab)-1][k] += tab[i][k]
-            tab[k][len(tab)-1] += tab[k][i]
-    return tab
+########## automate des tas de sable version sequentiel ##############
 
 
 def etape_sequentiel(tab):
@@ -55,7 +75,6 @@ def etape_sequentiel(tab):
     for i in range(1, len(tab)-3):
         for k in range(1, len(tab)-3):
             if tab_modif[i][k] >= 4:
-             
                 tab_modif[i][k] = tab[i][k] - 4
                 tab_modif[i][k-1] += 1
                 tab_modif[i][k+1] += 1
@@ -71,7 +90,11 @@ def automate_sequentiel(tab):
     res = etape_sequentiel(tab)
     while res[1]:
         res = etape_sequentiel(res[0])
-    return res[0]
+    res_vrai = somme(res[0])
+    return res_vrai
+
+
+####### automate des tas de sable version parallele ##########################
 
 
 def etape_parallele(tab):
@@ -85,8 +108,7 @@ def etape_parallele(tab):
 
     for i in range(1, len(tab)-3):
         for k in range(1, len(tab)-3):
-            if tab_modif[i][k] >= 4:
-             
+            if tab_modif[i][k] >= 4:      
                 tab_modif[i][k] = tab_modif[i][k] - 4
                 tab_modif[i][k-1] += 1
                 tab_modif[i][k+1] += 1
@@ -99,8 +121,7 @@ def etape_parallele(tab):
     else:
         return [tab, changement]
 
-
-def automate_parallele(tab):
+def automate_parallele1(tab):
     nom_fichier = input("entrer le nom du fichier (ne pas oublier .txt) ")
     res = etape_parallele(tab)
     while res[1]:
@@ -114,18 +135,21 @@ def automate_parallele(tab):
     return res[0]
 
 
-def creer_tableau_2():
-    choix = input("taper 'nouveau' si vous souhaitez charger un nouveau tableau et taper 'charger' sinon : ")
-    if choix == "charger":
-        nom_fichier = input("entrer le nom du fichier (ne pas oublier .txt) : ")
-        chargement = open(nom_fichier, "r")
-        list_chargement = [(line.strip()).split() for line in chargement]
-        chargement.close()
-        afficher_tableau(list_chargement)
-    elif choix == "nouveau":
-        n = input("entrer la taille du tableau (un entier) : ")
-        tableau = creer_tableau(int(n))
-        afficher_tableau(tableau)
+def automate_parallele(tab):
+    res = etape_parallele(tab)
+    while res[1]:
+        res = etape_parallele(res[0])
+    res_vrai = somme(res[0])
+    return res_vrai
 
 
-creer_tableau_2()
+############# commandes tests ###################
+
+test = creer_tableau(4)
+afficher_tableau(test)
+print("------------")
+test_para = automate_parallele(test)
+afficher_tableau(test_para)
+print("--------------")
+test_seq = automate_sequentiel(test)
+afficher_tableau(test_seq)
