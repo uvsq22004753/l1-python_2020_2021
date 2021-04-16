0b40260cbe384e0d2c744cdd310c12b57afdb3b7
Pauline HOSTI <pauline.hosti@ens.uvsq.fr>
2021-04-16 11:14:53 +0200

    m

--- exercises/projet - snake/brouillon_1_snake.py
+++ exercises/projet - snake/brouillon_1_snake.py
@@ -8,8 +8,20 @@ HEIGH = 600
 
 parois = tk.PhotoImage(file='muraille_40.gif')
 pomme = tk.PhotoImage(file='pomme_40.gif')
-tete_serpent = tk.PhotoImage(file='tete_serpent_40.gif')
-corps_serpent = tk.PhotoImage(file='corps_serpent_40.gif')
+tete_serpent_gauche = tk.PhotoImage(file='tete_serpent_gauche_40.gif')
+tete_serpent_droite = tk.PhotoImage(file='tete_serpent_droite_40.gif')
+tete_serpent_haut = tk.PhotoImage(file='tete_serpent_haut_40.gif')
+tete_serpent_bas = tk.PhotoImage(file='tete_serpent_bas_40.gif')
+corps_lateral = tk.PhotoImage(file='corps_lateral_40.gif')
+corps_vertical = tk.PhotoImage(file='corps_vertical_40.gif')
+queue_serpent_gauche = tk.PhotoImage(file='queue_serpent_gauche_40.gif')
+queue_serpent_droite = tk.PhotoImage(file='queue_serpent_droite_40.gif')
+queue_serpent_haut = tk.PhotoImage(file='queue_serpent_haute_40.gif')
+queue_serpent_bas = tk.PhotoImage(file='queue_serpent_bas_40.gif')
+tournant_droit_bas = tk.PhotoImage(file='tournant_droit_bas_40.gif')
+tournant_gauche_bas = tk.PhotoImage(file='tournant_gauche_bas_40.gif')
+tournant_droit_haut = tk.PhotoImage(file='tournant_droite_haut_40.gif') 
+tournant_gauche_haut = tk.PhotoImage(file='tournant_gauche_haut_40.gif')
 
 
 def decors():
@@ -22,8 +34,10 @@ def decors():
                 env_jeu.create_image(x, y, image=parois, anchor="nw")
             elif case == "P":
                 env_jeu.create_image(x, y, image=pomme, anchor="nw")
-            elif case == "S":
-                env_jeu.create_image(x, y, image=tete_serpent, anchor="nw")
+            elif case == "T":
+                env_jeu.create_image(x, y, image=tete_serpent_gauche, anchor="nw")
+            elif case == "Q":
+                env_jeu.create_image(x, y, image=queue_serpent_gauche, anchor="nw")
             x += 40
         x = 0
         y += 40
